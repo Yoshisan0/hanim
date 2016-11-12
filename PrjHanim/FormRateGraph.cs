c826e5cdb695d705318289ef37de3b93f4a94f01
yoshisan0 <yoshihara_t@asterios.biz>
2016-11-12 18:08:10 +0900

    重みのグラフを設定するフォームを追加しました

---
+++ PrjHanim/FormRateGraph.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PrjHikariwoAnim
+{
+    public partial class FormCurve : Form
+    {
+        public FormCurve()
+        {
+            InitializeComponent();
+        }
+
+        private void panel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
