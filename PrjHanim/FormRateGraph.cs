da0421e0b17d78fc963cd141335fa02eec637597
yoshisan0 <yoshihara_t@asterios.biz>
2016-11-13 16:25:43 +0900

    中間ポイントを移動できるように修正しました

--- PrjHanim/FormRateGraph.cs
+++ PrjHanim/FormRateGraph.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Windows.Forms;
 
@@ -11,25 +12,41 @@ namespace PrjHikariwoAnim
 {
     public partial class FormRateGraph : Form
     {
+        private static float SIZE_ELLIPSE = 15.0f;  //円の直径
         private bool mPush;
-        private int mX;
-        private int mY;
         private int mGridWidth;
         private Pen mPen;
         private Pen mPenGrid;
+        private Vector3[] mListPos;
+        private Vector3[] mListVec;
+        private int mGripNo;    //掴んでいるポイントの番号(-1:掴んでいない 0～2:番号)
 
         public FormRateGraph(int inGridWidth)
         {
             InitializeComponent();
 
             //以下、初期化処理
+            this.mGripNo = -1;
             this.mGridWidth = inGridWidth;
+
+            //panel_PreView.DoubleBuuferd = true;
+            panel_PreView.GetType().InvokeMember("DoubleBuffered", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.SetProperty, null, panel_PreView, new object[] { true });
         }
 
         private void FormRateGraph_Load(object sender, EventArgs e)
         {
-            this.mPen = new Pen(this.button_GraphColor.BackColor);
+            this.mPen = new Pen(this.button_GraphColor.BackColor, 2.0f);
             this.mPenGrid = new Pen(this.button_GridColor.BackColor);
+
+            this.mListPos = new Vector3[3];
+            this.mListPos[0] = new Vector3(0.0f, 1.0f, 0.0f);
+            this.mListPos[1] = new Vector3(0.5f, 0.5f, 0.0f);
+            this.mListPos[2] = new Vector3(1.0f, 0.0f, 0.0f);
+
+            this.mListVec = new Vector3[3];
+            this.mListVec[0] = new Vector3(0.0f, 0.0f, 0.0f);
+            this.mListVec[1] = new Vector3(0.0f, 0.0f, 0.0f);
+            this.mListVec[2] = new Vector3(0.0f, 0.0f, 0.0f);
 
             this.panel_PreView.Refresh();
         }
@@ -86,16 +103,16 @@ namespace PrjHikariwoAnim
 
         private void panel_PreView_Paint(object sender, PaintEventArgs e)
         {
+            int inCnt;
+
             //以下、背景クリア処理
             e.Graphics.Clear(this.button_BackColor.BackColor);
 
             //以下、グリッド描画処理
             if (this.checkBox_GridCheck.Checked) {
-                int inCnt;
-
                 //以下、縦ライン描画処理
-                float flSpan = (float)this.panel_PreView.Width / 10;
-                for (inCnt = 1; inCnt < 10; inCnt++)
+                float flSpan = (float)this.panel_PreView.Width / this.mGridWidth;
+                for (inCnt = 1; inCnt < this.mGridWidth; inCnt++)
                 {
                     e.Graphics.DrawLine(this.mPenGrid, inCnt * flSpan, 0.0f, inCnt * flSpan, this.panel_PreView.Height);
                 }
@@ -108,8 +125,24 @@ namespace PrjHikariwoAnim
                 }
             }
 
-            //以下、ペジェ曲線描画処理
-            e.Graphics.DrawLine(this.mPen, 0.0f, this.panel_PreView.Height, this.panel_PreView.Width, 0.0f);
+            //以下、ライン描画処理
+            float flX0 = this.mListPos[0].X * this.panel_PreView.Width;
+            float flY0 = this.mListPos[0].Y * this.panel_PreView.Height;
+            float flX1 = this.mListPos[1].X * this.panel_PreView.Width;
+            float flY1 = this.mListPos[1].Y * this.panel_PreView.Height;
+            e.Graphics.DrawLine(this.mPen, flX0, flY0, flX1, flY1);
+
+            flX0 = this.mListPos[1].X * this.panel_PreView.Width;
+            flY0 = this.mListPos[1].Y * this.panel_PreView.Height;
+            flX1 = this.mListPos[2].X * this.panel_PreView.Width;
+            flY1 = this.mListPos[2].Y * this.panel_PreView.Height;
+            e.Graphics.DrawLine(this.mPen, flX0, flY0, flX1, flY1);
+
+            //以下、ポイント描画処理
+            float flX = this.mListPos[1].X * this.panel_PreView.Width;
+            float flY = this.mListPos[1].Y * this.panel_PreView.Height;
+            Pen clPen = (this.mGripNo == 1) ? Pens.Red : this.mPen;
+            e.Graphics.DrawEllipse(clPen, flX - FormRateGraph.SIZE_ELLIPSE / 2, flY - FormRateGraph.SIZE_ELLIPSE / 2, FormRateGraph.SIZE_ELLIPSE, FormRateGraph.SIZE_ELLIPSE);
         }
 
         private void panel_PreView_MouseDown(object sender, MouseEventArgs e)
@@ -117,6 +150,18 @@ namespace PrjHikariwoAnim
             if (e.Button == MouseButtons.Left)
             {
                 this.mPush = true;
+
+                //以下、グリップ処理
+                this.mGripNo = -1;
+                float flX = this.mListPos[1].X * this.panel_PreView.Width;
+                float flY = this.mListPos[1].Y * this.panel_PreView.Height;
+                float flLen = (float)Math.Sqrt((e.X - flX) * (e.X - flX) + (e.Y - flY) * (e.Y - flY));
+                if (flLen < FormRateGraph.SIZE_ELLIPSE / 2)
+                {
+                    this.mGripNo = 1;
+                }
+
+                this.panel_PreView.Refresh();
             }
         }
 
@@ -124,8 +169,20 @@ namespace PrjHikariwoAnim
         {
             if (!this.mPush) return;
 
-            this.mX = e.X;
-            this.mY = e.Y;
+            if (this.mGripNo == 1)
+            {
+                int inX = e.X;
+                int inY = e.Y;
+                if (inX < 0) inX = 0;
+                if (inX > this.panel_PreView.Width) inX = this.panel_PreView.Width;
+                if (inY < 0) inY = 0;
+                if (inY > this.panel_PreView.Height) inY = this.panel_PreView.Height;
+
+                this.mListPos[this.mGripNo].X = (float)inX / this.panel_PreView.Width;
+                this.mListPos[this.mGripNo].Y = (float)inY / this.panel_PreView.Height;
+
+                this.panel_PreView.Refresh();
+            }
         }
 
         private void panel_PreView_MouseUp(object sender, MouseEventArgs e)
@@ -133,6 +190,9 @@ namespace PrjHikariwoAnim
             if (e.Button == MouseButtons.Left)
             {
                 this.mPush = false;
+                this.mGripNo = -1;
+
+                this.panel_PreView.Refresh();
             }
         }
     }
